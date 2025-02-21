df2acef3ce3ed072c87450af72298eab5398ec89
ahmed mostafa <engsp00ner51@gmail.com>
2025-02-21 20:13:31 +0200

    adding finalization comments

--- STORE/Context/FavouritesContext.js
+++ STORE/Context/FavouritesContext.js
@@ -1,21 +1,31 @@
 import { createContext, useState } from "react";
 
+//this is the context that will be used in the whole app to manage the favourites
 export const favouritesContext = createContext({
+  //list all stared IDS in this array
   ids: [],
+  //the functions that will be used to add and remove the favourites
   addFavourite: (id) => {},
   removeFavourite: (id) => {},
 });
+
 function favouritesContextProvider({ children }) {
   //this state is going to be used to collect all the favourite IDs in single list
-  const [favouriteMealIds, setFavouriteMealIds] = useState([]);
+  const [favouriteMealIds, setFavouriteMealIds] =
+    useState([]);
 
   function addFavourite(id) {
-    setFavouriteMealIds((currentFavIds) => [...currentFavIds, id]);
+    setFavouriteMealIds((currentFavIds) => [
+      ...currentFavIds,
+      id,
+    ]);
   }
 
   function removeFavourite(id) {
     setFavouriteMealIds((currentFavIds) =>
-      currentFavIds.filter((mealId) => mealId !== id)
+      currentFavIds.filter(
+        (mealId) => mealId !== id
+      )
     );
   }
   //this is how we pass the func and parameters to the provider
